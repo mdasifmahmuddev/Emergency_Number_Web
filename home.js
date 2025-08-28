a2e347e2db75e0c030d23111384903f3d1e0bde3
mdasifmahmuddev <md.asif.mahmud.dev@gmail.com>
2025-08-29 01:30:51 +0600

    copy event listener add

--- home.js
+++ home.js
@@ -4,14 +4,14 @@
 // heart count  
 
 const heartCount = document.getElementById("heartCount_id");
- 
-let count =0 ; 
-document.addEventListener("click",function(e){ 
 
-    if(e.target.classList.contains("heartt_class")){ 
-      count ++ ; 
-      heartCount.textContent =count ; 
-    }
+let count = 0;
+document.addEventListener("click", function (e) {
+
+  if (e.target.classList.contains("heartt_class")) {
+    count++;
+    heartCount.textContent = count;
+  }
 
 });
 
@@ -19,34 +19,59 @@ document.addEventListener("click",function(e){
 
 //set gold 100
 
-let goldCoins =100 ; 
-  
+let goldCoins = 100;
 
-document.getElementById('coin_id').innerText = goldCoins ;
+document.getElementById('coin_id').innerText = goldCoins;
 
 // alert  text 
 
- 
- document.addEventListener("click", function(e){ 
-  if (e.target.classList.contains("call_button_class") ) {
-    
-    let card =e.target.parentNode.parentNode.parentNode;
- 
-    let serviceName= card.querySelector(".service-name").innerText ;
-     let serviceNumber=card.querySelector(".service_number").innerText;
+//  call history
 
-    if(goldCoins >= 20 ){ 
-      goldCoins -= 20 ; 
-    document.getElementById('coin_id').innerText=goldCoins;
-    
-    
+const historyAll = document.querySelector(".call-history-container");
+const clearButton = document.querySelector(".clear-history-btn");
+
+
+
+
+
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("call_button_class")) {
+
+    let card = e.target.parentNode.parentNode.parentNode;
+
+    let serviceName = card.querySelector(".service-name").innerText;
+    let serviceNumber = card.querySelector(".service_number").innerText;
+
+    if (goldCoins >= 20) {
+      goldCoins -= 20;
+
+      // left side form 
+      let now = new Date();
+      let timeString = now.toLocaleTimeString();
+
+      // create  div
+      let entry = document.createElement("div");
+      entry.classList.add("flex", "justify-between", "mb-4");
+      entry.innerHTML = `
+        <div>
+          <h1>${serviceName}</h1>
+          <p class="text-[#5C5C5C]">${serviceNumber}</p>
+        </div>
+        <div>${timeString}</div>
+      `;
+      historyAll.prepend(entry);
+
+      document.getElementById('coin_id').innerText = goldCoins;
+
+
       alert(`📞 Calling: ${serviceName} ${serviceNumber} `);
 
 
 
     }
-    
-    else { 
+
+    else {
       alert("❌ আপনার পর্যাপ্ত কয়েন নেই ! কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
     }
 
@@ -57,5 +82,35 @@ document.getElementById('coin_id').innerText = goldCoins ;
 
 
 
- 
 
+
+clearButton.addEventListener("click", function () {
+  historyAll.innerHTML = "";
+});
+
+
+
+// copy id increment 
+
+ 
+let copyCount = 0;
+
+document.addEventListener("click", function (e) {
+  if (e.target.closest("button") && e.target.closest("button").textContent.includes("Copy")) {
+    let card = e.target.closest(".m-3");  
+    let serviceNumber = card.querySelector(".service_number").innerText;
+
+     
+    navigator.clipboard.writeText(serviceNumber).then(() => {
+      
+      copyCount++;
+
+       
+      document.getElementById("copy_id").innerText = copyCount;
+            alert(`নম্বর কপি হয়েছে: ${serviceNumber}`);
+
+    }).catch(err => {
+      console.error("Copy failed", err);
+    });
+  }
+});
